59126883199ec17b08a0bf8619030e688da2709f
RJSYS Soft Solution <sumritarora.512@gmail.com>
2023-05-10 11:42:05 +0530

    changes

--- apps/web/next-seo.config.ts
+++ apps/web/next-seo.config.ts
@@ -1,17 +1,17 @@
 import { DefaultSeoProps } from 'next-seo'
 
 export const SEO: DefaultSeoProps = {
-  titleTemplate: '%s | Uniexchange',
-  defaultTitle: 'Uniexchange',
+  titleTemplate: '%s | UniExchange',
+  defaultTitle: 'UniExchange',
   description:
-    'Cheaper and faster than Uniswap? Discover Uniexchange, the leading DEX on BNB Smart Chain (BSC) with the best farms in DeFi and a lottery for MNB.',
+    'Cheaper and faster than Uniswap? Discover UniExchange, the leading DEX on BNB Smart Chain (BSC) with the best farms in DeFi and a lottery for MNB.',
   twitter: {
     cardType: 'summary_large_image',
-    handle: '@Uniexchange',
-    site: '@Uniexchange',
+    handle: '@UniExchange',
+    site: '@UniExchange',
   },
   openGraph: {
-    title: '🥞 Uniexchange - A next evolution DeFi exchange on BNB Smart Chain (BSC)',
+    title: '🥞 UniExchange - A next evolution DeFi exchange on BNB Smart Chain (BSC)',
     description:
       'The most popular AMM on BSC by user count! Earn MNB through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by PancakeSwap), NFTs, and more, on a platform you can trust.',
     images: [{ url: 'https://mnb3d.com/MNB_logo.png' }],
